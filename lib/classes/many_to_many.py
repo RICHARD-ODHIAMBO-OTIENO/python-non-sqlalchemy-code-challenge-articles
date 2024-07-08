43b809cb0f88893c13e67b02990c1dbd0187bbb6
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-07-08 20:38:30 +0300

    pYTHON phase 3 code challenge_Articles draft commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,98 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
-        self.name = name
-
-    def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+        self._name = name
+        self._articles = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name is immutable")
 
     def add_article(self, magazine, title):
-        pass
-
+        new_article = Article(self, magazine, title)
+        self._articles.append(new_article)
+        return new_article
+    
+    def articles(self):
+        return self._articles
+    
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+    
     def topic_areas(self):
-        pass
+        return list(set(article.magazine.category for article in self._articles))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-
+        self._name = name
+        self._category = category
+        self._articles = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if not 2 <= len(value) <= 16:
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string")
+        if len(value) == 0:
+            raise ValueError("Category must have a length greater than 0")
+        self._category = value
+    
+    def add_article(self, article):
+        self._articles.append(article)
+    
     def articles(self):
-        pass
-
+        return self._articles
+    
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
-    def article_titles(self):
-        pass
 
-    def contributing_authors(self):
-        pass
+class Article:
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        magazine.add_article(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title is immutable")
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        self._author = value
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, value):
+        self._magazine = value
